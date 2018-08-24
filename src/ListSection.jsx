af38ecb0c26fa92c7c847cae6f06cf38e39e2d25
jpbaena13 <jpbaena13@gmail.com>
2018-08-23 20:04:19 -0500

    Converting component in funcional component

--- src/ListSection.jsx
+++ src/ListSection.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-class ListSection extends React.Component {
-  render() {
-    return (
-      <div className="unc-list-section">
-        <h4 className="unc-audio-title">{this.props.title}</h4>
-        {this.props.children.map((child, idx) => (
+function ListSection(props) {
+  return (
+    <div className="unc-list-section">
+        <h4 className="unc-audio-title">{props.title}</h4>
+        {props.children.map((child, idx) => (
           React.cloneElement(child, {
             key: idx,
-            onClickItem: this.props.onClickItem,
-            currentItem: this.props.currentItem,
-            file: this.props.file,
-            sectionTitle: this.props.title,
-            items: this.props.items
+            onClickItem: props.onClickItem,
+            currentItem: props.currentItem,
+            file: props.file,
+            sectionTitle: props.title,
+            items: props.items
           })
         ))}
       </div>
-    );
-  }
+  );
 }
 
 export default ListSection;
